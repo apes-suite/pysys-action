c8d10ea00a1206e40d1e3078320e4e3462000b28
Harald Klimach <harald.klimach@dlr.de>
2025-05-21 23:53:59 +0200

    Make GITHUB_OUTPUT accessible for apes

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,6 @@ echo "Copying project to user workspace"
 mkdir -p /home
 cp -rfL . /home/apes
 chown -R apes /home/apes
-ls -ld $GITHUB_OUTPUT
 projectfile=$(find /home/apes -name pysysproject.xml)
 if [[ "$projectfile" == "" ]]; then
   echo "ERROR! Could not find pysysproject.xml." >> $GITHUB_STEP_SUMMARY
@@ -21,6 +20,9 @@ fi
 testroot=$(dirname $projectfile)
 ORIGIN=$PWD
 cd $testroot/$1
+OUSER=$(ls -ld $GITHUB_OUTPUT | awk 'NR==1 {print $3}')
+OGROUP=$(ls -ld $GITHUB_OUTPUT | awk 'NR==1 {print $4}')
+chown -R apes $GITHUB_OUTPUT
 echo "::endgroup::"
 echo "::group::Testing"
 echo "Running pysys.py run --ci --outdir=CItests in $1"
@@ -28,6 +30,7 @@ runuser -u apes -- pysys.py run --ci --outdir=CItests
 SUCCESS=$?
 echo "::endgroup::"
 echo "::group::Test-Finalization"
+chown -R $OUSER:$OGROUP $GITHUB_OUTPUT
 cd $ORIGIN
 cp -rfL /home/apes $ORIGIN
 if [[ $SUCCESS ]]; then
